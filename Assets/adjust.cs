128960345d52e6b21f56abcfb217219f74bdea08
Colin <zephyr.raine@gmail.comp>
2020-09-10 14:59:18 +0200

    push

---
+++ Assets/adjust.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adjust : MonoBehaviour
+{
+    public RectTransform canvas;
+
+    // Update is called once per frame
+    public void AdjustSize(float v)
+    {
+        var cur = (transform as RectTransform).localPosition;
+        cur.z = 0.5f*v;
+        (transform as RectTransform).localPosition = cur;
+    }
+}
