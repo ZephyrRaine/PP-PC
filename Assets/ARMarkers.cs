bc6221dfd49a9e1bb896b68906650595ab142005
Colin <zephyr.raine@gmail.comp>
2020-09-17 11:48:32 +0200

    ok

--- Assets/ARMarkers.cs
+++ Assets/ARMarkers.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class ARMarkers : MonoBehaviour
 {
     public Transform markersTransform;
-
-    public void NextStep()
+    public Transform tipsTransform;
+    int nbScreen = 2;
+    int current = 0;
+    private void Awake()
     {
-       
+        ChangeCurrentFood(0);
     }
 
-    public void PreviousStep()
+    public void ChangeCurrentFood(int offset)
     {
+        current += offset;
+        if (current < 0)
+            current = 2;
+        if (current > 2)
+            current = 0;
+
+        for (int i = 0; i < markersTransform.childCount; i++)
+        {
+            Debug.Log("going through transform " + i);
+            markersTransform.GetChild(i).gameObject.SetActive(i == current);
+            tipsTransform.GetChild(i).gameObject.SetActive(i == current);
+        }
 
     }
 }
---
+++ Assets/ScrollMarkers.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMarkers : MonoBehaviour
+{
+    ARMarkers _marks;
+    ARMarkers marks
+    {
+        get
+        {
+            if (_marks == null)
+                _marks = FindObjectOfType<ARMarkers>();
+
+            return _marks;
+        }
+    }
+
+    public void Previous()
+    {
+        marks?.ChangeCurrentFood(-1);
+    }
+    public void Next()
+    {
+        marks?.ChangeCurrentFood(1);
+    }
+}
